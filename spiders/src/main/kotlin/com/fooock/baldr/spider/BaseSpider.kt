995e9df49fcecf9e0a1375ca39dbbe9386ef5b6f
Javi <nhitbh@gmail.com>
2018-09-06 16:07:23 +0200

    Added start url's and allowed domain to the BaseSpider class

--- spiders/src/main/kotlin/com/fooock/baldr/spider/BaseSpider.kt
+++ spiders/src/main/kotlin/com/fooock/baldr/spider/BaseSpider.kt
@@ -3,4 +3,14 @@ package com.fooock.baldr.spider
 /**
  * Base class for all spiders
  */
-abstract class BaseSpider(val name: String)
+abstract class BaseSpider(val name: String) {
+    /**
+     * Initial url's for this Spider to be crawled
+     */
+    abstract val startUrls: Array<String>
+
+    /**
+     * Array of allowed domains to be crawled by this spider.
+     */
+    open val allowedDomains: Array<String> = emptyArray()
+}
--- spiders/src/test/kotlin/com/fooock/baldr/spider/BaseSpiderTest.kt
+++ spiders/src/test/kotlin/com/fooock/baldr/spider/BaseSpiderTest.kt
@@ -1,5 +1,6 @@
 package com.fooock.baldr.spider
 
+import org.junit.Assert.assertArrayEquals
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -7,12 +8,19 @@ import org.junit.Test
  *
  */
 class BaseSpiderTest {
+    private val fakeSpider = FakeSpider()
 
     @Test
-    fun testSpiderName() {
-        val fakeSpider = FakeSpider()
-        assertEquals("fake-spider", fakeSpider.name)
-    }
+    fun testSpiderName() = assertEquals("fake-spider", fakeSpider.name)
+
+    @Test
+    fun testEmptyAllowedDomains() = assertArrayEquals(emptyArray(), fakeSpider.allowedDomains)
+
+    @Test
+    fun testEmptyStartUrls() = assertArrayEquals(emptyArray(), fakeSpider.startUrls)
 }
 
-class FakeSpider : BaseSpider("fake-spider")
+class FakeSpider : BaseSpider("fake-spider") {
+    override val startUrls: Array<String> = emptyArray()
+    override val allowedDomains: Array<String> = emptyArray()
+}
